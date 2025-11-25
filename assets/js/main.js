2be5b9a893383486e82ee8f0df2ea3c70fe13140
Muhammad Ayoub Khan <ayoubkhan558@hotmail.com>
2025-11-25 12:43:46 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     duration: 1.2,
     smoothWheel: true,
     smoothTouch: true,
-    lerp: 0.5,
+    lerp: 0.075,
   });
 
   function raf(time) {
